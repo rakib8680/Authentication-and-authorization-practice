00c0f77ab4bad95f4c429d1a8a52e8bb6c469c8d
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-27 17:44:37 +0600

    news data added

--- the-dragon-news-server/index.js
+++ the-dragon-news-server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 const cors = require('cors')
 
 app.get('/', (req, res) => {
@@ -14,6 +15,16 @@ app.use(cors())
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(news.filter(item => item._id === id));
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
