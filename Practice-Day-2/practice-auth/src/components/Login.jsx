fecd8efb232d4df83326aa9eb752c8157d82c568
rakib8680 <rakib.cst.1st@gmail.com>
2023-04-30 20:45:52 +0600

    registration form done

--- Practice-Day-2/practice-auth/src/components/Login.jsx
+++ Practice-Day-2/practice-auth/src/components/Login.jsx
@@ -1,42 +1,46 @@
 import React from 'react';
 
 const Login = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-900">
-      <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-white font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline-gray"
-            id="email"
-            type="text"
-            placeholder="Email"
-          />
+
+    
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-900">
+            <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-white font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline-gray"
+                        id="email"
+                        type="text"
+                        name='email'
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-white font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline-gray"
+                        id="password"
+                        type="password"
+                        name='password'
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+                        type="button"
+                    >
+                        Sign In
+                    </button>
+                </div>
+            </form>
         </div>
-        <div className="mb-6">
-          <label className="block text-white font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline-gray"
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
-            type="button"
-          >
-            Sign In
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- Practice-Day-2/practice-auth/src/components/Register.jsx
+++ Practice-Day-2/practice-auth/src/components/Register.jsx
@@ -2,8 +2,61 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            register
+        <div className="flex justify-center items-center h-screen bg-gray-900">
+            <form className="bg-gray-800 shadow-md rounded px-10 py-8 mb-4 w-96">
+                <div className="mb-4">
+                    <label className="block text-white font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline-gray"
+                        id="username"
+                        type="text"
+                        placeholder="Username"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-white font-bold mb-2" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline-gray"
+                        id="email"
+                        type="text"
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-white font-bold mb-2" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline-gray"
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="mb-6">
+                    <label className="block text-white font-bold mb-2" htmlFor="confirm-password">
+                        Confirm Password
+                    </label>
+                    <input
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none focus:shadow-outline-gray"
+                        id="confirm-password"
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+                        type="button"
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
         </div>
     );
 };
